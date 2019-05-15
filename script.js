a9f489a3c45e5f5dcf6338fa38e6e8a9d5fba78d
claudia <claucanchin@gmail.com>
2019-05-15 20:26:00 +0800

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 class List extends React.Component {
+    render() {
+        console.log("list propssss:",this.props.data);
+
+        let list = this.props.data.map((todo, index) => {
+            return <li key={ index }>{ todo }</li>
+        });
+        return (
+            <ol>
+                {list}
+            </ol>
+        );
+    }
+}
+
+
+class App extends React.Component {
   constructor(){
     super()
     this.changeHandler = this.changeHandler.bind( this );
+    this.clickHandler = this.clickHandler.bind( this );
   }
 
   state = {
@@ -11,24 +28,38 @@ class List extends React.Component {
 
   changeHandler(event){
     this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+    // console.log("change", event.target.value);
+  }
+
+  clickHandler(event){
+    let inputText = this.state.word;
+    this.state.list.push(inputText);
+    this.setState({
+        list : this.state.list,
+        word : ""
+    })
   }
 
   render() {
       // render the list with a map() here
+      // return this.state.list.map((todo, index) => {
+
+      // })
 
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <input onChange={ this.changeHandler } value={this.state.word}/>
+          <button onClick={ this.clickHandler }>add item</button>
+            <List data = { this.state.list } />
         </div>
       );
   }
 }
 
 ReactDOM.render(
-    <List/>,
+    <App
+
+    />,
     document.getElementById('root')
 );
-
